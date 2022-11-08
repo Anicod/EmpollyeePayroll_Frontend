1173de5f5ea99925811556fded807619d68849e3
Anicod <animeshraj735@gmail.com>
2022-11-09 04:40:58 +0530

    added UI of Empolyee Payroll using Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdduserComponent } from './component/adduser/adduser.component';
+import { EmployeeComponent } from './component/employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: EmployeeComponent},
+  {path: 'adduser', component: AdduserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { AdduserComponent } from './component/adduser/adduser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    AdduserComponent
   ],
   imports: [
     BrowserModule,
