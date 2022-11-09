eed049f076eb679add05086446c24300a3d59292
Anicod <animeshraj735@gmail.com>
2022-11-09 16:54:48 +0530

    added employee payroll form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './component/employee/employee.component';
 import { AdduserComponent } from './component/adduser/adduser.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AdduserComponent } from './component/adduser/adduser.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/adduser/adduser.component.ts
+++ src/app/component/adduser/adduser.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adduser.component.css']
 })
 export class AdduserComponent implements OnInit {
-
+  userName = ""
+  nameErr = ""
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onInput($event:any){
+    console.log("hello", $event.data)
+    const nameRegx = RegExp('[A-Z]{1}[a-zA-Z\\s]{2,}$')
+    if(nameRegx.test(this.userName)){
+      this.nameErr = ""
+      return;
+    }
+      this.nameErr = "name is not valid"
   }
 
 }
